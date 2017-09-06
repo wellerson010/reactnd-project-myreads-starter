de733bd4f00b986e78ba500a1e54bdb24b321b4b
Wellerson <wellerson010@hotmail.com>
2017-09-05 22:46:51 -0300

    refactor
    Ajustes do componente book e seu container
    
    Iniciando trabalhos com a API

--- src/components/book/book.js
+++ src/components/book/book.js
@@ -1,27 +1,37 @@
 import React from 'react';
-import styles from './book.css';
+import styles from './Book.css';
 
-const Book = ({ title, authors, imageUrl, status, showSelectStatus }) => (
-    <div className={styles.book}>
-        <div className={styles.bookTop}>
-            <div className={styles.cover}
-                style={{ backgroundImage: 'url("' + imageUrl + '")' }}>
+const Book = ({
+    title, authors, imageUrl, status, showSelectStatus, onClickContainerSelectStatus,
+    onClickSelectStatus, onChangeSelectStatus }) => (
+        <div className={styles.book}>
+            <div className={styles.bookTop}>
+                <div className={styles.cover}
+                    style={{ backgroundImage: 'url("' + imageUrl + '")' }}>
+                </div>
+                <div className={styles.containerSelectStatus} onClick={onClickContainerSelectStatus}>
+                    {
+                        showSelectStatus &&
+                        <select
+                            onClick={onClickSelectStatus}
+                            onChange={function (evt) {
+                                onChangeSelectStatus(evt.target.value);
+                            }}
+                            className={styles.selectStatus}
+                            value={status}
+                        >
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    }
+                </div>
             </div>
-            <div className={styles.containerSelectStatus}>
-                {
-                    showSelectStatus && <select className={styles.selectStatus} value={status}>
-                        <option value="none" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead" selected>Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                    </select>
-                }
-            </div>
+            <div className="book-title">{title}</div>
+            <div className={styles.bookAuthors}>{authors}</div>
         </div>
-        <div className="book-title">{title}</div>
-        <div className="book-authors">{authors}</div>
-    </div>
-);
+    );
 
 export default Book;
--- src/containers/book.js
+++ src/containers/book.js
@@ -1,14 +1,51 @@
 import React from 'react';
-import BookComponent from '../components/book/book';
+
+import BookComponent from '../components/Book/Book';
 
 export default class Book extends React.Component {
+    state = {
+        showSelectStatus: false,
+        status: 'none'
+    };
+
+    constructor(props) {
+        super();
+        
+        if (props.state) {
+            this.state.status = this.props.status;
+        } 
+    }
+
+    handleChangeSelectStatus = (value) => {
+        this.setState({
+            status: value
+        });
+    }
+
+    handleClickContainerSelectStatus = () => {
+        this.setState((prevState) => {
+            return {
+                showSelectStatus: !prevState.showSelectStatus
+            }
+        })
+    }
+
+    handleClickSelectStatus = (evt) => {
+        evt.stopPropagation();
+    }
+
     render() {
         return (
             <BookComponent
                 title="Primeiro teste"
                 authors="Autor"
+                status={this.state.status}
+                showSelectStatus={this.state.showSelectStatus}
+                onClickContainerSelectStatus={this.handleClickContainerSelectStatus}
+                onClickSelectStatus={this.handleClickSelectStatus}
+                onChangeSelectStatus={this.handleChangeSelectStatus}
                 imageUrl="http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api"
             />
-        )
+        );
     }
 }
