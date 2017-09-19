130f7dd9c4592c531ff5afcc8c53f948c5006b81
Wellerson <wellerson010@hotmail.com>
2017-09-19 15:01:21 -0300

    refactor
    Colocado o componente App na estrutura padrão do projeto

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import AppComponent from '../components/App/App';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <AppComponent />
+        )
+    }
+}
